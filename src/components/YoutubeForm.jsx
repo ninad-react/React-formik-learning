96bfd86ab4076eba7b2d3f85c4887ebd5a8c3cf3
ninad-react <157863834+ninad-react@users.noreply.github.com>
2024-02-21 16:12:13 +0530

    using Field Component

--- src/components/YoutubeForm.jsx
+++ src/components/YoutubeForm.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Form, Formik } from 'formik'
+import { Field, Form, Formik } from 'formik'
 import * as yup from "yup"
 
 const initialValues = {
@@ -33,33 +33,30 @@ const YoutubeForm = () => {
         <Form>
           <div className='form-control'>
               <label htmlFor='name'>Name</label>
-              <input 
+              <Field 
                 type='text' 
                 id='name' 
                 name='name' 
-                {...formik.getFieldProps('name')}
               />
               {formik.touched.name && formik.errors.name ? <div className='error'>{formik.errors.name}</div> : null}
             </div>
 
             <div className='form-control'>
               <label htmlFor='name'>E-mail</label>
-              <input 
+              <Field 
                 type='text' 
                 id='email' 
                 name='email' 
-                {...formik.getFieldProps('email')}
               />
               {formik.touched.email && formik.errors.email ? <div className='error'>{formik.errors.email}</div> : null}
             </div>
 
             <div className='form-control'>
               <label htmlFor='channel'>Channel</label>
-              <input 
+              <Field 
                 type='text' 
                 id='channel' 
                 name='channel'
-                {...formik.getFieldProps('channel')}
               />
               {formik.touched.channel && formik.errors.channel ? <div className='error'>{formik.errors.channel}</div> : null}
             </div>
